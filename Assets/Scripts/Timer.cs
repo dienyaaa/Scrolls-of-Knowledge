350a9ac0bb8df22fd95e9bf3051c87b73c5c07d5
dienyaaa <40639641+dienyaaa@users.noreply.github.com>
2020-08-30 23:36:12 +0500

    new!!

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timeStart = 600f;
+    public Text timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer.text = timeStart.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeStart -= Time.deltaTime;
+        timer.text = Mathf.Round(timeStart).ToString();
+    }
+}
